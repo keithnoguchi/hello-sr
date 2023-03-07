0a6a49f78efbb7ebb023dcfbb5b0e1849ed1008c
Keith Noguchi <keith@noguchi.us>
2023-03-06 18:28:45 -0800

    Add some documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 //! A [hello world] contract on [Soroban] platform
 //!
 //! [hello world]: https://soroban.stellar.org/docs/getting-started/hello-world
-//! [soroban]: https://soroban.stellar.org/
+//! [soroban]: https://soroban.stellar.org/docs
 
 #![no_std]
 
 use soroban_sdk::{contractimpl, symbol, vec, Env, Symbol, Vec};
 
+/// A hello world contract.
+#[derive(Debug)]
 pub struct Hello;
 
 #[contractimpl]
 impl Hello {
+    /// Returns a vector with `Hello` appended to `to` argument.
     pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
         vec![&env, symbol!("Hello"), to]
     }
