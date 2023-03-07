71e4dedfb62d47734f64ff87aabfc4216af86a81
Keith Noguchi <keith@noguchi.us>
2023-03-06 18:40:11 -0800

    counter example to demonstrate the contract store

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,21 @@ use soroban_sdk::{contractimpl, symbol, vec, Env, Symbol, Vec};
 #[derive(Debug)]
 pub struct Hello;
 
+const COUNTER: Symbol = symbol!("COUNTER");
+
 #[contractimpl]
 impl Hello {
     /// Returns a vector with `Hello` appended to `to` argument.
     pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
         vec![&env, symbol!("Hello"), to]
+    }
+
+    /// Increments a counter by '1' and returns the result.
+    pub fn increment(env: Env) -> u32 {
+        let mut count: u32 = env.storage().get(&COUNTER).unwrap_or(Ok(0)).unwrap();
+        count += 1;
+        env.storage().set(&COUNTER, &count);
+        count
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -2,11 +2,25 @@ use super::{Hello, HelloClient};
 use soroban_sdk::{symbol, vec, Env};
 
 #[test]
-fn test() {
+fn hello() {
     let env = Env::default();
     let contract_id = env.register_contract(None, Hello);
     let client = HelloClient::new(&env, &contract_id);
 
     let got = client.hello(&symbol!("Soroban"));
     assert_eq!(got, vec![&env, symbol!("Hello"), symbol!("Soroban")],);
+}
+
+#[test]
+fn increment() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Hello);
+    let client = HelloClient::new(&env, &contract_id);
+
+    let got = client.increment();
+    assert_eq!(got, 1);
+    let got = client.increment();
+    assert_eq!(got, 2);
+    let got = client.increment();
+    assert_eq!(got, 3);
 }
